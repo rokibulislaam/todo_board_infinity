dd28d020002850baa2caf358ac9c2f27dcda15a5
Rokibul Islam <rokibul630@gmail.com>
2022-09-09 20:48:43 +0530

    📚 Add README

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## TODO App REST API with job scheduler
+The server uses Agenda to schedule jobs, i.e. delete the todo tasks from the database.
+
+There was an issue with deploying swagger UI on vercel, so it has been again deployed on AWS EC2 instance with docker. Please view it live at: http://3.110.217.146:3002/#/tasks/get_list
--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ export class App {
   private initMiddlewares() {
     this.app.use(morgan(LOG_FORMAT || 'dev', { stream }));
     this.app.use(cors({ origin: ORIGIN }));
-    this.app.use(helmet());
+    // this.app.use(helmet());
     this.app.use(compression());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
